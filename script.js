1e64edea419e0c1766c22dbfb82a90b08d3ee25b
bestsleeper <isieun258@gmail.com>
2025-04-04 17:45:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById('search').addEventListener('input', function() {
-    console.log("검색어: ", this.value);
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search');
+    
+    searchInput.addEventListener('input', function(event) {
+        console.log('검색어:', event.target.value);
+    });
 });
